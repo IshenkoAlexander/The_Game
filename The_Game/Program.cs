d8ac9883fbc5fa13cbe2d39a16e1e210a3b53c02
User <User@DESKTOP-AEHS4BU>
2024-06-13 20:11:36 +0700

    The_player's_move

--- The_Game/Program.cs
+++ The_Game/Program.cs
@@ -10,6 +10,44 @@ namespace The_Game
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ваш выбор:");
+            Console.WriteLine("1. Камень");
+            Console.WriteLine("2. Ножницы");
+            Console.WriteLine("3. Бумага");
+            Console.WriteLine("0. Выход");
+            string choice = "";
+            do
+            {
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Ваш выбор: Камень\t-");
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Ваш выбор: Ножницы\t-");
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Ваш выбор: Бумага\t-");
+                            break;
+                        }
+                    case "0":
+                        {
+                            Console.WriteLine("До свидания =)");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Некорректный ввод данных");
+                            break;
+                        }
+                }
+            } while (choice != "0");
         }
     }
 }
